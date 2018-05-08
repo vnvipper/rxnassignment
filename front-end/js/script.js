c3c2f2fefe61726effb8ac62952ecba46a7b7a47
nguyen pham <nguyenpvdtu@gmail.com>
2018-05-08 21:26:16 +0700

    create new if not exist

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -6,7 +6,19 @@
         init: function () {
             $('#productNumber').select2({
                 placeholder: "Select a Product Number",
-                allowClear: true
+                tags: true,
+                tokenSeparators: [",", " "]
+            }).on("change", function (e) {
+                var isNew = $(this).find('[data-select2-tag="true"]');
+                if (isNew.length) {
+                    var r = confirm("do you want to create a tag?");
+                    if (r == true) {
+                        isNew.replaceWith('<option selected value="' + isNew.val() + '">' + isNew.val() + '</option>');
+                    } else {
+                        $('.select2-selection__choice:last').remove();
+                        $('.select2-search__field').val(isNew.val()).focus()
+                    }
+                }
             });
 
             $('#prePack').select2({
@@ -96,59 +108,77 @@
     // };
 
     var imageHandler = {
-        init: function(){
+        init: function () {
             function readURL(input) {
                 if (input.files && input.files[0]) {
                     var reader = new FileReader();
-                    
+
                     reader.onload = function (e) {
                         $('#imagePreview').attr('src', e.target.result);
                         var base64result = e.target.result.split(',')[1];
                         $('#productImage').val(base64result);
                     }
-                    
+
                     reader.readAsDataURL(input.files[0]);
                 }
             }
 
-            $('#btn-browser').on('click', function(){
+            $('#btn-browser').on('click', function () {
                 $("#inputfile").trigger("click");
             });
 
-            $("#inputfile").change(function(){
+            $("#inputfile").change(function () {
                 readURL(this);
-            }); 
+            });
+
+            $('#btn-clear').on('click', function () {
+                var defaultImage = './img/placeholder.png';
+                $('#imagePreview').attr('src', defaultImage);
+                $('#productImage').val(null);
+            });
         }
     }
 
     var jqueryGridHandler = {
-        load: function(){
+        load: function () {
             var dataUrl = siteUri + 'Inventory' + '/Mockdata';
-            $("#jqGrid").jqGrid  
-            ({  
-                url: dataUrl,  
-                datatype: 'json',  
-                mtype: 'get',  
-                styleUI : 'Bootstrap',
-                emptyrecords: 'No records',  
-                colNames: ['Location', 'On-Hand', 'On-Hand Pcs', 'Allocated', 'Allocated Pcs', 'Available', 'Available Pcs'], 
-                colModel: [  
-                { name: "location" },  
-                { name: "onHand" },  
-                { name: "onHandPcs" },  
-                { name: "allocated" },  
-                { name: "allocatedPcs" },  
-                { name: "available" },  
-                { name: "availablePcs" }
-                ],  
+            $("#jqGrid").jqGrid({
+                url: dataUrl,
+                datatype: 'json',
+                mtype: 'get',
+                styleUI: 'Bootstrap',
+                emptyrecords: 'No records',
+                colNames: ['Location', 'On-Hand', 'On-Hand Pcs', 'Allocated', 'Allocated Pcs', 'Available', 'Available Pcs'],
+                colModel: [{
+                        name: "location"
+                    },
+                    {
+                        name: "onHand"
+                    },
+                    {
+                        name: "onHandPcs"
+                    },
+                    {
+                        name: "allocated"
+                    },
+                    {
+                        name: "allocatedPcs"
+                    },
+                    {
+                        name: "available"
+                    },
+                    {
+                        name: "availablePcs"
+                    }
+                ],
                 viewrecords: true,
                 rowNum: 20,
                 pager: "#jqGridPager",
-                rowNum: 10,  
-                rowList: [10, 20, 30, 40],  
-                footerrow : true,
-                userDataOnFooter : true,
-                loadComplete: function(){
+                rowNum: 10,
+                rowList: [10, 20, 30, 40],
+                footerrow: true,
+                userDataOnFooter: true,
+                loadComplete: function () {
                     // footer data
                     $(this).jqGrid("footerData", "set", {
                         location: "ALL SITE:",
@@ -160,100 +190,109 @@
                         availablePcs: $(this).jqGrid('getCol', 'availablePcs', false, 'sum')
                     });
                 },
-            }); 
-        }, 
-        reload: function(){
+            });
+        },
+        reload: function () {
             var itemMasterId = $('#grid-link').attr('data-master-id');
-            if(itemMasterId.length){
-                var dataUrl = siteUri + 'Inventory' + '?masterId='+ itemMasterId;
-                $("#jqGrid").jqGrid().setGridParam({url : dataUrl}).trigger("reloadGrid");
-            }
-            else{
+            if (itemMasterId.length) {
+                var dataUrl = siteUri + 'Inventory' + '?masterId=' + itemMasterId;
+                $("#jqGrid").jqGrid().setGridParam({
+                    url: dataUrl
+                }).trigger("reloadGrid");
+            } else {
                 var dataUrl = siteUri + 'Inventory' + '/Mockdata';
-                $("#jqGrid").jqGrid().setGridParam({url : dataUrl}).trigger("reloadGrid");
+                $("#jqGrid").jqGrid().setGridParam({
+                    url: dataUrl
+                }).trigger("reloadGrid");
             }
         }
     }
 
     var siteHandler = {
-        init: function(){
+        init: function () {
 
-            $('#isPrePack').on('change', function(){
+            $('#isPrePack').on('change', function () {
                 var isCheck = $(this).prop("checked");
-                $('#prePack').prop( "disabled", !isCheck );
+                $('#prePack').prop("disabled", !isCheck);
             });
 
             $('#isPrePack').trigger('change');
 
             var self = this;
-            self.loadProductNumber();    
-            self.onSelectedProductNumber();   
-            self.onClickInventoryTab();  
+            self.loadProductNumber();
+            self.onSelectedProductNumber();
+            self.onClickInventoryTab();
         },
-        loadProductNumber: function(){
+        loadProductNumber: function () {
             var masterItemUri = siteUri + 'Master';
             $.ajax({
-                type: 'get',
-                url: masterItemUri,
-                dataType: 'json',
-                contentType: 'application/json',
-            })
-            .done(function (data) {
-                $('#productNumber').val(null).trigger('change');
-                $.each( data, function( key, value ) {
-                    var newOption = new Option(value.productNumber, value.productNumber, false, false);
-                    $('#productNumber').append(newOption).trigger('change');
-                });
-            })
-            .fail(function (jqXHR, textStatus, errorThrown) {
-                alert(errorThrown);
-            });     
-        },
-        onSelectedProductNumber: function(){
-            $('#productNumber').on('select2:select', function (e) {
-                var masterItemUri = siteUri + 'Master' + '/' + $(this).val();
-                $.ajax({
                     type: 'get',
                     url: masterItemUri,
                     dataType: 'json',
                     contentType: 'application/json',
                 })
                 .done(function (data) {
-                    $('#grid-link').attr('data-master-id', data.productNumber);
-                    $('#packSize').val(data.packSize);
-                    $('#description').val(data.description);
-
-                    if(data.productImage){
-                        $('#productImage').val(data.productImage);
-                        $('#imagePreview').attr('src',data.imagePreview);
-                    }
-                    else {
-                        var defaultImage = './img/placeholder.png';
-                        $('#imagePreview').attr('src',defaultImage);
-                    }
-
-                    $('#isHazardousMaterial').prop( "checked", data.isHazardousMaterial );
-
-                    $('#costCenterCode').val(data.costCenterCode);
-                    $('#expirationDate').val(data.expirationDate);
-                    $('#unitPrice').val(data.unitPrice);
-                    $('#width').val(data.width);
-                    $('#length').val(data.length);
-                    $('#height').val(data.height);
-                    $('#cube').val(data.cube);
-                    $('#isPrePack').prop("checked", data.isPrePack);
-                    $('#isPrePack').trigger('change');
-                    $('#prePack').val(data.prePack);
-                    $('#prePack').trigger('change');
+                    $('#productNumber').val(null).trigger('change');
+                    $.each(data, function (key, value) {
+                        var newOption = new Option(value.productNumber, value.productNumber, false, false);
+                        $('#productNumber').append(newOption).trigger('change');
+                    });
                 })
                 .fail(function (jqXHR, textStatus, errorThrown) {
                     alert(errorThrown);
-                });   
+                });
+        },
+        onSelectedProductNumber: function () {
+            $('#productNumber').on('select2:select', function (e) {
+                var masterItemUri = siteUri + 'Master' + '/' + $(this).val();
+                $.ajax({
+                        type: 'get',
+                        url: masterItemUri,
+                        dataType: 'json',
+                        contentType: 'application/json',
+                    })
+                    .done(function (data) {
+                        // Select tab by name
+                        $('.nav-tabs a[href="#basic-info"]').tab('show')
+                        $('#grid-link').attr('data-master-id', data.productNumber);
+                        $('#packSize').val(data.packSize);
+                        $('#description').val(data.description);
+
+                        if (data.productImage) {
+                            $('#productImage').val(data.productImage);
+                            $('#imagePreview').attr('src', data.imagePreview);
+                        } else {
+                            var defaultImage = './img/placeholder.png';
+                            $('#imagePreview').attr('src', defaultImage);
+                        }
+
+                        $('#isHazardousMaterial').prop("checked", data.isHazardousMaterial);
+
+                        $('#costCenterCode').val(data.costCenterCode);
+                        $('#expirationDate').val(data.expirationDate);
+                        $('#unitPrice').val(data.unitPrice);
+                        $('#width').val(data.width);
+                        $('#length').val(data.length);
+                        $('#height').val(data.height);
+                        $('#cube').val(data.cube);
+                        $('#isPrePack').prop("checked", data.isPrePack);
+                        $('#isPrePack').trigger('change');
+                        $('#prePack').val(data.prePack);
+                        $('#prePack').trigger('change');
+
+                    })
+                    .fail(function (jqXHR, textStatus, errorThrown) {
+                        alert(errorThrown);
+                    });
             });
         },
-        onClickInventoryTab: function(){
-            $('#grid-link').on('click', function(){
-                jqueryGridHandler.reload();
+        onClickInventoryTab: function () {
+            $('#grid-link').on('click', function () {
+                var li = $(this).closest('li');
+                console.log(li);
+                if (!li.hasClass('active')) {
+                    jqueryGridHandler.reload();
+                }
             });
         }
     }
